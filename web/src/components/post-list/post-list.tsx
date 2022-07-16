d59e7a316f53f531422f0bc7fdbca76c187a0646
Kwangtae Kim <zabra7946@naver.com>
2022-07-17 01:25:20 +0900

    create post-list component

---
+++ web/src/components/post-list/post-list.tsx
@@ -0,0 +1,26 @@
+import './post-list.css';
+
+interface Post {
+  id: string;
+  title: string;
+}
+
+interface PostListProps {
+  posts: Post[];
+}
+
+const PostList: React.FC<PostListProps> = ({ posts }) => {
+  return (
+    <div className="post-list">
+      {posts.map((post) => (
+        <div key={post.id} className="post">
+          <h3>{post.title}</h3>
+          <div>commentlist</div>
+          <div>commentcreate</div>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default PostList;
