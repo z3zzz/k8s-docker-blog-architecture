c347bd775bd5fa8d90ce33d1e16e5561754ae840
Kwangtae Kim <zabra7946@naver.com>
2022-07-17 21:48:12 +0900

    refactor root component

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,31 +1,18 @@
-import { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
-import PostList, { Post } from './components/post-list';
-import PostCreate from './components/post-create';
-import * as Api from './utils/api';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Post from './components/post';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
 const App: React.FC = () => {
-  const [posts, setPosts] = useState<Post[]>([]);
-
-  useEffect(() => {
-    const getPosts = async () => {
-      const { data } = await Api.get('/posts');
-      setPosts(data);
-    };
-
-    getPosts();
-  }, []);
-
   return (
-    <div>
-      <PostCreate setPosts={setPosts} />
-      <hr />
-      <PostList posts={posts} />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Post />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 
