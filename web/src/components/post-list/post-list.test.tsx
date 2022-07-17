2af7e024af48696d9b33eadb06d69d639a3f0d32
Kwangtae Kim <zabra7946@naver.com>
2022-07-17 19:24:11 +0900

    fix test condition

--- web/src/components/post-list/post-list.test.tsx
+++ web/src/components/post-list/post-list.test.tsx
@@ -1,12 +1,11 @@
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import '@testing-library/jest-dom';
-import Header from './header';
+import PostList from './post-list';
 
 describe('Header Component', () => {
   it('should render header', async () => {
     const user = userEvent.setup();
-    render(<Header color="red" />);
 
     const basicElement = await screen.findByText('header');
     await user.click(basicElement);
