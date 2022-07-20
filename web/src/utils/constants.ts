d53d05ca542b0b8d8e69369815404f9de3011bfd
z3zzz <zabra7946@naver.com>
2022-07-20 07:25:44 +0000

    fix typo and environment variables

--- web/src/utils/constants.ts
+++ web/src/utils/constants.ts
@@ -4,4 +4,4 @@ export const commentApiOrigin =
   process.env.REACT_APP_COMMENT_API_ORIGIN || 'http://localhost:5001';
 export const queryApiOrigin =
   process.env.REACT_APP_QUERY_API_ORIGIN || 'http://localhost:5002';
-export const environment = process.env.REACT_APP_ENVIRONMENT || 'development';
+export const environment = process.env.NODE_ENV
