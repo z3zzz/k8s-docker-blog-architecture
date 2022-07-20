b2094bf3a8faacf011be9c1571f4374d0354be07
Kwangtae Kim <zabra7946@naver.com>
2022-07-20 11:47:39 +0900

    store events for sync process

--- event-bus/src/event-bus.ts
+++ event-bus/src/event-bus.ts
@@ -11,21 +11,28 @@ import {
   moderationApiOrigin,
 } from './constants';
 
+interface Event {
+  eventType:
+    | 'PostCreated'
+    | 'CommentCreated'
+    | 'CommentUpdated'
+    | 'CommentModerated';
+  eventData: any;
+}
+
 interface PostEvent {
-  Body: {
-    eventType:
-      | 'PostCreated'
-      | 'CommentCreated'
-      | 'CommentUpdated'
-      | 'CommentModerated';
-    eventData: any;
-  };
+  Body: Event;
   Reply: {
     result: 'success';
   };
 }
 
+interface GetEvents {
+  Reply: Event[];
+}
+
 const SUBSCRIBERS = [commentApiOrigin, queryApiOrigin, moderationApiOrigin];
+const events: Event[] = [];
 
 export async function eventBusRoutes(
   router: FastifyInstance,
@@ -48,7 +55,7 @@ export async function eventBusRoutes(
         },
       },
       response: {
-        200: {
+        201: {
           type: 'object',
           properties: {
             result: { type: 'string', pattern: 'success' },
@@ -58,23 +65,45 @@ export async function eventBusRoutes(
     },
   };
 
-  router.post<PostEvent>('/event', opts['post-event'], (req, res) => {
+  router.post<PostEvent>('/event', opts['post-event'], async (req, res) => {
     const { eventType, eventData } = req.body;
+    events.push({ eventType, eventData });
 
     console.log({ eventType, eventData });
 
     for (const origin of SUBSCRIBERS) {
       try {
-        axios.post(`${origin}/event`, {
+        await axios.post(`${origin}/event`, {
           eventType,
           eventData,
         });
       } catch (e: any) {
-        console.error(`Error occured in Post ${origin}: ${e.message}`);
+        req.log.error(`Error occured for Post to ${origin}: ${e.message}`);
       }
     }
 
     res.code(201);
     return { result: 'success' };
+  });
+
+  opts['get-events'] = {
+    schema: {
+      response: {
+        200: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              eventType: { type: 'string' },
+              eventData: {},
+            },
+          },
+        },
+      },
+    },
+  };
+
+  router.get<GetEvents>('/events', opts['get-events'], (req, res) => {
+    return events;
   });
 }
